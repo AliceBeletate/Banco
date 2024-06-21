187ac591914c47223292adf80131c0a2898d62f4
Alice Beletate <beletatealice@gmail.com>
2024-06-21 19:36:13 -0300

    first commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,9 @@
+public class ContaCorrente extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'imprimirExtrato'");
+    }
+ 
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,9 @@
+public class ContaPoupanca extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'imprimirExtrato'");
+    }
+
+}
